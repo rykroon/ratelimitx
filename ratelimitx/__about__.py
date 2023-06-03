67ac97cc3eb72ac8ed09177e64d8fbd7749f496c
Ryan Kroon <rykroon.tech@gmail.com>
2023-06-03 10:55:20 -0400

    Added list of ideas.

--- ratelimitx/__about__.py
+++ ratelimitx/__about__.py
@@ -2,3 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.2.0"
+
+"""
+    Ideas
+
+    - Add __call__() method that acts as a decorator.
+
+    - Add logic to timsim generator so that you can send elapsed time
+        back to the generator. ??
+
+    - Add sync versions of RateLimiter and MultiRateLimiter.
+
+    - Add support fpr python 3.7 and 3.8.
+"""
